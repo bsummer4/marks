6357584b6f3fec5c59dc4b5fdd9f44bf40eb98e5
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-10-10 21:38:15 -0400

    Fixed a bug, and made error messages consistaint.

--- marks.c
+++ marks.c
@@ -17,7 +17,7 @@ static void header (char *l)
 {	assert('#'==*l);
 	int c;
 	for (c=0; '#'==*l; c++,l++);
-	if (' '!=(*l++)) errx(1, "Invalid Header");
+	if (' '!=(*l++)) errx(1, "Invalid Header.");
 	printf("<h%d>%s</h%d>\n", c, l, c); }
 
 static void endpara () { if (Inpara[Level]) puts("</p>"),Inpara[Level]=false; }
@@ -26,15 +26,16 @@ static void startpara () { if (!Inpara[Level]) puts("<p>"),Inpara[Level]=true; }
 static void pedant (char *line)
 {	for (char *c=line; *c; c++)
 		 if ('\t'==*c)
-			errx(1, "No tabs are allowed except at the beginning of lines");
+			errx(1, "No tabs are allowed except at the beginning of lines.");
 	{	int l = strlen(line);
 		if (l && isspace(line[l-1]))
-			errx(1, "No spaces are allowed at the end of a line"); }}
+			errx(1, "No spaces are allowed at the end of a line."); }}
 
 /* Sets 'Level', handles <blockquote>s, and removed tabs from l.  */
 static void handlequote (char **l)
 {	int c;
 	for (c=0; '\t'==**l; c++,(*l)++);
+	if (c>=7) errx(1, "Only 6 levels of indentation are allowed.");
 	while (Level>c) puts("</blockquote>"),Level--;
 	while (Level<c) puts("<blockquote>"),Level++,Inpara[Level]=true; }
 
@@ -53,10 +54,10 @@ int main (int argc, char *argv[])
 	while (buf[78] = '\0', fgets(buf, 80, stdin)) /* strlen(buf)>0 */
 		if (buf[78] == '\n' || buf[78] == '\0')
 		{	int l=strlen(buf);
-			if ('\n'!=buf[l-1]) errx(1, "Last character must be a newline.  ");
+			if ('\n'!=buf[l-1]) errx(1, "Last character must be a newline.");
 			buf[l-1]='\0';
 			input(buf); }
-		else errx(1, "A line is longer than 78 characters");
+		else errx(1, "A line is longer than 78 characters.");
 	input("");
 	puts("</body></html>");
 	return 0; }
