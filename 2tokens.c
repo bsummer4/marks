8573dd44b9d08fe470da4eb2244af4b42582a7e0
Benjamin Summers <bsummer4@eecs.utk.edu>
2011-06-29 20:44:15 -0400

    Nicer-looking output from [2tokens.c].

--- 2tokens.c
+++ 2tokens.c
@@ -12,17 +12,17 @@ void hdr() { puts("HDR"); }
 void nlist() { puts("NLIST"); }
 void anno() { puts("ANNO"); }
 
-void item(char *l) { printf("ITEM: %s\n", l); }
+void item(char *l) { printf("ITEM=%s\n", l); }
 void bullet() { puts("BULLET"); }
-void hdrdepth(int i) { printf("HDRDEPTH: %d\n",i); }
+void hdrdepth(int i) { printf("HDRDEPTH=%d\n",i); }
 void qline(char *line) { printf("\t%s\n",line); }
 void word(Word w) {
 	char *pre="";
 	switch(w.t) {
-	case QUOTED: pre="QUOTED WORD\n\t"; break;
-	case PLAIN: pre="PLAIN WORD\n\t"; break;
-	case LINK: pre="LINK\n\t"; break;
-	case GROUP: w.w="{"; break;
-	case ENDGROUP: w.w="}"; break;
-	case WS: w.w="SPACE"; break; }
+	case QUOTED: pre="QWORD\t"; break;
+	case PLAIN: pre="PLAIN\t"; break;
+	case LINK: pre="LINK\t"; break;
+	case GROUP: w.w="\t{"; break;
+	case ENDGROUP: w.w="\t}"; break;
+	case WS: w.w="WS"; break; }
 	printf("%s%s\n", pre, w.w); }
